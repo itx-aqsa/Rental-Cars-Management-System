c2d6d7a90b3a7fe95b79f36a60fa5d9b9ecad31a
Zaeem <zaeemahmad2603@gmail.com>
2023-10-17 16:40:03 +0500

    Create a page for employee to manage reservations

---
+++ src/Components/Employee/Reservations_Employee.js
@@ -0,0 +1,131 @@
+import React from 'react'
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Fab from "@mui/material/Fab";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const Reservations_Employee = () => {
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    {
+      _id: "651c2e64a6724252ca540847",
+      name: "zain",
+      password: "73000",
+      email: "zain@gmail.com",
+      contact: "030921212",
+      address: "Lahore",
+      username: "zain",
+      role: "Employee",
+      active: true,
+      createdDate: "2023-10-03T15:08:20.600Z",
+      updatedDate: "2023-10-03T15:08:20.601Z",
+      __v: 0,
+    },
+    {
+      _id: "651c65cadd36f193bd35e923",
+      name: "ahmad",
+      password: "123",
+      email: "ahmad@gmail.com",
+      contact: "030921212",
+      address: "Lahore",
+      username: "ahmad",
+      role: "Employee",
+      active: true,
+      createdDate: "2023-10-03T19:04:42.987Z",
+      updatedDate: "2023-10-03T19:04:42.987Z",
+      __v: 0,
+    },
+    {
+      _id: "651c65e5dd36f193bd35e925",
+      name: "bilal",
+      password: "56464",
+      email: "goodbilal@gmail.com",
+      contact: "1234567823",
+      address: "Multan",
+      username: "bilal",
+      role: "Employee",
+      active: true,
+      createdDate: "2023-10-03T19:05:09.112Z",
+      updatedDate: "2023-10-07T06:02:28.756Z",
+      __v: 0,
+    },
+  ];
+
+
+return (
+<div>
+      <div className="First_Line">
+        <h2 style={{ marginLeft: "4%" }}>Reservations</h2>
+      </div>
+      <div className="">
+        <TableContainer component={Paper} style={{width: '90%', marginLeft: '4%', marginTop: '50px'}} >
+          <Table sx={{ }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell style={{fontSize: '17px', fontWeight: '500'}} >Customer</StyledTableCell>
+                <StyledTableCell style={{fontSize: '17px', fontWeight: '500'}} >Vehicle No.</StyledTableCell>
+                <StyledTableCell style={{fontSize: '17px', fontWeight: '500'}} >Start Date</StyledTableCell>
+                <StyledTableCell style={{fontSize: '17px', fontWeight: '500'}} >End Date</StyledTableCell>
+                <StyledTableCell style={{fontSize: '17px', fontWeight: '500'}} >Total Cost</StyledTableCell>
+                <StyledTableCell style={{fontSize: '17px', fontWeight: '500'}} >Confirm</StyledTableCell>
+                <StyledTableCell style={{fontSize: '17px', fontWeight: '500'}} >Cancel</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell style={{fontSize: '15px'}} component="th" scope="row">
+                    {row.name}
+                  </StyledTableCell>
+                  <StyledTableCell style={{fontSize: '15px'}} >{row.email}</StyledTableCell>
+                  <StyledTableCell style={{fontSize: '15px'}} >{row.contact}</StyledTableCell>
+                  <StyledTableCell style={{fontSize: '15px'}} >{row.username}</StyledTableCell>
+                  <StyledTableCell style={{fontSize: '15px'}} >{row.address}</StyledTableCell>
+                  <StyledTableCell style={{fontSize: '15px'}} >
+                  <Button variant="contained" style={{backgroundColor: 'red', width: '80%', textTransform: 'capitalize'}}>Confirm</Button>  
+                  </StyledTableCell>
+                  <StyledTableCell style={{fontSize: '15px'}} >
+                  <Button variant="contained" style={{backgroundColor: 'red', width: '80%', textTransform: 'capitalize'}}>Cancel</Button>    
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  )
+}
+
+export default Reservations_Employee
