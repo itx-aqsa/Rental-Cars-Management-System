8237cfdb41ea6b2dfd9da4e444e5fddff2bde228
Zaeem <zaeemahmad2603@gmail.com>
2023-11-12 03:30:50 +0500

    Develop fully integrated vehicles list page for admin

--- src/Components/Admin/Vehicles_Admin.js
+++ src/Components/Admin/Vehicles_Admin.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import Table from "@mui/material/Table";
@@ -11,8 +11,24 @@ import Paper from "@mui/material/Paper";
 import Fab from "@mui/material/Fab";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from '@mui/icons-material/Delete';
+import { useNavigate } from 'react-router-dom';
+import { VehicleContext } from '../../Context/AllContexts';
 
 const Vehicles_Admin = () => {
+    const navigate = useNavigate();
+
+    const Context = useContext(VehicleContext)
+    const { vehicles, setVehicles, fetchAllVehicles, removeVehicle, vehicleToBeEdited, setVehicleToBeEdited } = Context
+
+
+    useEffect(() => {
+      if (localStorage.getItem("token")) {
+        fetchAllVehicles()
+      } else {
+        alert("You have to Logged In into the system.");
+        navigate("/Login");
+      }
+    }, []);
 
     const StyledTableCell = styled(TableCell)(({ theme }) => ({
         [`&.${tableCellClasses.head}`]: {
@@ -33,55 +49,13 @@ const Vehicles_Admin = () => {
           border: 0,
         },
       }));
-    
-      function createData(name, calories, fat, carbs, protein) {
-        return { name, calories, fat, carbs, protein };
+
+
+      const RemovePresentVehicle = (id) => {
+        // alert(id)
+        alert("Deleted Successfully!")
+        removeVehicle(id)
       }
-    
-      const rows = [
-        {
-          _id: "651c2e64a6724252ca540847",
-          name: "zain",
-          password: "73000",
-          email: "zain@gmail.com",
-          contact: "030921212",
-          address: "Lahore",
-          username: "zain",
-          role: "Employee",
-          active: true,
-          createdDate: "2023-10-03T15:08:20.600Z",
-          updatedDate: "2023-10-03T15:08:20.601Z",
-          __v: 0,
-        },
-        {
-          _id: "651c65cadd36f193bd35e923",
-          name: "ahmad",
-          password: "123",
-          email: "ahmad@gmail.com",
-          contact: "030921212",
-          address: "Lahore",
-          username: "ahmad",
-          role: "Employee",
-          active: true,
-          createdDate: "2023-10-03T19:04:42.987Z",
-          updatedDate: "2023-10-03T19:04:42.987Z",
-          __v: 0,
-        },
-        {
-          _id: "651c65e5dd36f193bd35e925",
-          name: "bilal",
-          password: "56464",
-          email: "goodbilal@gmail.com",
-          contact: "1234567823",
-          address: "Multan",
-          username: "bilal",
-          role: "Employee",
-          active: true,
-          createdDate: "2023-10-03T19:05:09.112Z",
-          updatedDate: "2023-10-07T06:02:28.756Z",
-          __v: 0,
-        },
-      ];
 
 
   return (
@@ -103,6 +77,7 @@ const Vehicles_Admin = () => {
                 marginTop: "36px",
                 marginRight: "6%",
               }}
+              onClick={()=>{navigate('/admin/addvehicle')}}
             >
               Add Vehicle
             </Button>
@@ -122,15 +97,15 @@ const Vehicles_Admin = () => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {rows.map((row) => (
-                    <StyledTableRow key={row.name}>
+                  {vehicles.map((row) => (
+                    <StyledTableRow key={row._id}>
                       <StyledTableCell style={{fontSize: '15px'}} component="th" scope="row">
-                        {row.name}
+                        {row.brand.name}
                       </StyledTableCell>
-                      <StyledTableCell style={{fontSize: '15px'}} >{row.email}</StyledTableCell>
-                      <StyledTableCell style={{fontSize: '15px'}} >{row.contact}</StyledTableCell>
-                      <StyledTableCell style={{fontSize: '15px'}} >{row.username}</StyledTableCell>
-                      <StyledTableCell style={{fontSize: '15px'}} >{row.address}</StyledTableCell>
+                      <StyledTableCell style={{fontSize: '15px'}} >{row.year}</StyledTableCell>
+                      <StyledTableCell style={{fontSize: '15px'}} >{row.status}</StyledTableCell>
+                      <StyledTableCell style={{fontSize: '15px'}} >{row.plateNumber}</StyledTableCell>
+                      <StyledTableCell style={{fontSize: '15px'}} >{row.discount}</StyledTableCell>
                       <StyledTableCell>
                         <Fab
                           style={{
@@ -139,6 +114,22 @@ const Vehicles_Admin = () => {
                             width: "38px",
                           }}
                           aria-label="edit"
+                          onClick={()=>{
+                            setVehicleToBeEdited({
+                              id: row._id,
+                              brand: row.brand,
+                              year: row.year,
+                              plateNumber: row.plateNumber,
+                              image: row.image,
+                              description: row.description,
+                              status: row.status,
+                              discount: row.discount
+                            })
+                            console.log(vehicleToBeEdited)
+                            navigate('/admin/editVehicle')
+                            
+                          }
+                          }
                         >
                           <EditIcon />
                         </Fab>
@@ -151,6 +142,7 @@ const Vehicles_Admin = () => {
                             width: "38px",
                           }}
                           aria-label="edit"
+                          onClick={()=>{RemovePresentVehicle(row._id)}}
                         >
                           <DeleteIcon />
                         </Fab>
