3f864d096d20ccfafd5df86b47a067d5090c35e1
Zaeem <zaeemahmad2603@gmail.com>
2023-11-12 03:26:35 +0500

    Develop fully integrated edit active employee page for admin

---
+++ src/Components/Admin/Edit_Employee_Admin.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+
+
+const Edit_Employee_Admin = () => {
+
+    
+    
+      return (
+        <></>
+  )
+}
+
+export default Edit_Employee_Admin
