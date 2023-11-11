16fa02f4812b01528c821651bf3216df8923a236
Zaeem <zaeemahmad2603@gmail.com>
2023-11-12 03:27:59 +0500

    Develop fully integrated add new brand page for admin

--- src/Components/Admin/Add_Brand_Admin.js
+++ src/Components/Admin/Add_Brand_Admin.js
@@ -1,34 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect, useContext }  from 'react'
 import Button from "@mui/material/Button";
 import '../../Add_Brand.css'
+import { useNavigate } from 'react-router-dom';
+import { BrandContext } from '../../Context/AllContexts'
 
 const Add_Brand_Admin = () => {
+  const navigate = useNavigate();
+  const Context = useContext(BrandContext)
+  const { brands, addNewBrand } = Context;
+
+  const [newBrand, setNewBrand] = useState({
+    name: "",
+    description: ""
+  })
+
+  useEffect(() => {
+    if(localStorage.getItem('token')){
+      
+    }
+    else{
+      alert("You have to Logged In into the system.")
+      navigate('/Login')
+    }
+  }, [])
+
+  const AddNewBrand = (e) => {
+    e.preventDefault();
+    addNewBrand(newBrand.name, newBrand.description)
+    alert("Brand Added Successfully!")
+    navigate('/admin/brands')
+  }
+
+  const OnChange = (e) => {
+    setNewBrand({...newBrand, [e.target.name]: e.target.value })
+  }
+
+
+
     return (
         <div>
           <h2 style={{ marginLeft: "4%", textAlign: "center", marginBottom: '60px' }}>Add Brand</h2>
-          <form action="">
+          <form action="" onSubmit={AddNewBrand}>
             <div className="Name_Field">
-              <h4 className="name_label">Name</h4>
+              <h4 className="nameaddbrand_label">Name</h4>
               <input
                 className="name_input"
                 type="text"
                 placeholder="Enter the Name"
+                value={newBrand.name}
+                name='name'
+                onChange={OnChange}
+                required
+                minLength={3}
               />
             </div>
             
             <div className="Name_Field">
-              <h4 className="description_label">Description</h4>
+              <h4 className="descriptionaddbrand_label">Description</h4>
               <textarea
                 className="name_input"
                 type="text"
                 placeholder="Enter the Description"
+                value={newBrand.description}
+                name='description'
+                onChange={OnChange}
+                required
+                minLength={3}
                 style={{ fontFamily: "sans-serif", paddingTop: "4px" }}
               />
             </div>
             
-            <div className="button">
+            <div className="buttonaddbrand">
               <Button
-                type="submit"
                 variant="contained"
                 style={{
                   backgroundColor: "red",
@@ -38,6 +81,7 @@ const Add_Brand_Admin = () => {
                   borderRadius: "5px",
                   textTransform: "uppercase",
                 }}
+                onClick={()=>{navigate('/admin/brands')}}
               >
                 Cancel
               </Button>
