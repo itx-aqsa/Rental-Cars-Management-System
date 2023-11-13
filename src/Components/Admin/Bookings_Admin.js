acbb1d4501c1f7e0ff77c5c51e7794af0944e822
Zaeem <zaeemahmad2603@gmail.com>
2023-11-14 01:15:46 +0500

    Develop fully integrated bookings page for admin

--- src/Components/Admin/Bookings_Admin.js
+++ src/Components/Admin/Bookings_Admin.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import Table from "@mui/material/Table";
@@ -11,8 +11,24 @@ import Paper from "@mui/material/Paper";
 import Fab from "@mui/material/Fab";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from '@mui/icons-material/Delete';
+import { ReservationContext } from '../../Context/AllContexts';
+import { useNavigate } from 'react-router-dom';
 
 const Bookings_Admin = () => {
+    const navigate = useNavigate();
+
+    const Context = useContext(ReservationContext)
+    const { confirmedReservations, setConfirmedReservations, fetchAllConfirmedReservations } = Context
+
+    useEffect(() => {
+      if (localStorage.getItem("token")) {
+        fetchAllConfirmedReservations();
+      } else {
+        alert("You have to Logged In into the system.");
+        navigate("/Login");
+      }
+    }, []);
+  
 
     const StyledTableCell = styled(TableCell)(({ theme }) => ({
         [`&.${tableCellClasses.head}`]: {
@@ -33,56 +49,6 @@ const Bookings_Admin = () => {
           border: 0,
         },
       }));
-    
-      function createData(name, calories, fat, carbs, protein) {
-        return { name, calories, fat, carbs, protein };
-      }
-    
-      const rows = [
-        {
-          _id: "651c2e64a6724252ca540847",
-          name: "zain",
-          password: "73000",
-          email: "zain@gmail.com",
-          contact: "030921212",
-          address: "Lahore",
-          username: "zain",
-          role: "Employee",
-          active: true,
-          createdDate: "2023-10-03T15:08:20.600Z",
-          updatedDate: "2023-10-03T15:08:20.601Z",
-          __v: 0,
-        },
-        {
-          _id: "651c65cadd36f193bd35e923",
-          name: "ahmad",
-          password: "123",
-          email: "ahmad@gmail.com",
-          contact: "030921212",
-          address: "Lahore",
-          username: "ahmad",
-          role: "Employee",
-          active: true,
-          createdDate: "2023-10-03T19:04:42.987Z",
-          updatedDate: "2023-10-03T19:04:42.987Z",
-          __v: 0,
-        },
-        {
-          _id: "651c65e5dd36f193bd35e925",
-          name: "bilal",
-          password: "56464",
-          email: "goodbilal@gmail.com",
-          contact: "1234567823",
-          address: "Multan",
-          username: "bilal",
-          role: "Employee",
-          active: true,
-          createdDate: "2023-10-03T19:05:09.112Z",
-          updatedDate: "2023-10-07T06:02:28.756Z",
-          __v: 0,
-        },
-      ];
-
 
   return (
     <div>
@@ -102,15 +68,15 @@ const Bookings_Admin = () => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {rows.map((row) => (
-                    <StyledTableRow key={row.name}>
+                  {confirmedReservations.map((row) => (
+                    <StyledTableRow key={row._id}>
                       <StyledTableCell style={{fontSize: '15px'}} component="th" scope="row">
-                        {row.name}
+                        {row.customer.name}
                       </StyledTableCell>
-                      <StyledTableCell style={{fontSize: '15px'}} >{row.email}</StyledTableCell>
-                      <StyledTableCell style={{fontSize: '15px'}} >{row.contact}</StyledTableCell>
-                      <StyledTableCell style={{fontSize: '15px'}} >{row.username}</StyledTableCell>
-                      <StyledTableCell style={{fontSize: '15px'}} >{row.address}</StyledTableCell>
+                      <StyledTableCell style={{fontSize: '15px'}} >{row.vehicle.plateNumber}</StyledTableCell>
+                      <StyledTableCell style={{fontSize: '15px'}} >{row.startDate}</StyledTableCell>
+                      <StyledTableCell style={{fontSize: '15px'}} >{row.endDate}</StyledTableCell>
+                      <StyledTableCell style={{fontSize: '15px'}} >{row.totalCost}</StyledTableCell>
                     </StyledTableRow>
                   ))}
                 </TableBody>
