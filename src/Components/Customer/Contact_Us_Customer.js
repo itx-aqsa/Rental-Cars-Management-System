674270f1241b26a9b1baf4363531e627d7bff70a
Zaeem <zaeemahmad2603@gmail.com>
2023-11-16 23:42:00 +0500

    Develop fully integrated contact us page for customer

--- src/Components/Customer/Contact_Us_Customer.js
+++ src/Components/Customer/Contact_Us_Customer.js
@@ -1,16 +1,70 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Button from "@mui/material/Button";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
 import "../../ContactUs.css";
+import { useNavigate } from "react-router-dom";
+import { VehicleContext, FeedbackContext, CustomerContext } from "../../Context/AllContexts";
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+import StarIcon from '@mui/icons-material/Star';
 
 const Contact_Us_Customer = () => {
+  const navigate = useNavigate();
+  const [value, setValue] = React.useState(1);
+
+  const [newFeedback, setNewFeedback] = useState({
+    vehicle: "",
+    rating: "",
+    message: "",
+  });
+
+  const vehiclecontext = useContext(VehicleContext);
+  const feedbackcontext = useContext(FeedbackContext);
+  const customercontext = useContext(CustomerContext)
+
+  const { vehicles, setVehicles, fetchAllVehicles } = vehiclecontext;
+  const { feedbacks, setFeedbacks, addNewFeedback } = feedbackcontext;
+  const { getCustomer, setGetCustomer, GetCustomerData } = customercontext;
+
+  useEffect(() => {
+    if (localStorage.getItem("customerToken")) {
+      GetCustomerData();
+      fetchAllVehicles();
+    } else {
+      alert("You have to Logged In into the system.");
+      navigate("/Login");
+    }
+  }, []);
+
+  const dropdownFunction = (event) => {
+    vehicles.map((item) => {
+      if (item.plateNumber === event.target.value) {
+        setNewFeedback({...newFeedback, vehicle: item._id})
+        // alert(item._id);
+      }
+    });
+  };
+
+  const OnChange = (e) => {
+    setNewFeedback({...newFeedback, [e.target.name]: e.target.value})
+  }
+
+  const handleContactUs = (e) => {
+    e.preventDefault();
+    addNewFeedback(getCustomer._id, newFeedback.vehicle, newFeedback.rating, newFeedback.message)
+    setNewFeedback({
+      vehicle: "",
+      rating: "",
+      message: "",
+    })
+  }
+
   return (
     <div className="Main_Page">
-      <div className="one_portion">
+      <div className="one2_portion">
         <h2 className="Contact_Us_Heading">Contact Us :- </h2>
         <h5 className="Contact_Us_Paragaraph">
           If you want to get in touch with or contact us or give your feedback
@@ -20,72 +74,60 @@ const Contact_Us_Customer = () => {
           feedback on the performance of the car.
         </h5>
       </div>
-      <div className="contact_portion">
+      <div className="contact2_portion">
         <div>
           <h2 style={{ marginLeft: "4%", textAlign: "center" }}>
             Form for Contact Us
           </h2>
-          <form action="">
+          <form action="" onSubmit={handleContactUs}>
             <div className="Name_Field">
-              <h4 className="name_label">Name</h4>
+              <h4 className="namecontact_label">Name</h4>
               <input
                 className="name_input"
                 type="text"
                 placeholder="Enter your Name"
+                value={getCustomer.name}
+                required
+                readOnly
+                name="customer"
               />
             </div>
             <div className="Name_Field">
-              <h4 className="email_label">Email</h4>
+              <h4 className="emailcontact_label">Email</h4>
               <input
                 className="name_input"
                 type="email"
                 placeholder="Enter your Email"
+                value={getCustomer.email}
+                required
+                readOnly
+                name="email"
               />
             </div>
             <div className="Name_Field">
               <h4 className="carno_label">Car No.</h4>
-              <input
-                className="name_input"
-                type="text"
-                placeholder="Enter the Car Plate Number"
-              />
+              <select onChange={dropdownFunction} className="name_input">
+                <option value="" disabled selected>
+                  Select the Car Plate No.
+                </option>
+                {vehicles.map((element) => {
+                  return <option>{element.plateNumber}</option>;
+                })}
+              </select>
             </div>
             <div className="Name_Field">
-              <h4 className="rating_label">Rating</h4>
-              {/* <FormControl className="radio_forms"> */}
-                <RadioGroup
-                  row
-                  aria-labelledby="demo-row-radio-buttons-group-label"
-                  name="row-radio-buttons-group"
-                  style={{marginTop: '12px'}}
-                >
-                  <FormControlLabel
-                    value="1"
-                    control={<Radio />}
-                    label="1"
-                  />
-                  <FormControlLabel
-                    value="2"
-                    control={<Radio />}
-                    label="2"
-                  />
-                  <FormControlLabel
-                    value="3"
-                    control={<Radio />}
-                    label="3"
-                  />
-                  <FormControlLabel
-                    value="4"
-                    control={<Radio />}
-                    label="4"
-                  />
-                  <FormControlLabel
-                    value="5"
-                    control={<Radio />}
-                    label="5"
-                  />
-                  
-                </RadioGroup>
+              <h4 className="ratingcontact_label">Rating</h4>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                  setNewFeedback({...newFeedback, rating: newValue})
+                }}
+                required
+                size="100px"
+                style={{ marginTop: "22px" }}
+              />
               {/* </FormControl> */}
             </div>
             <div className="Name_Field">
@@ -94,13 +136,16 @@ const Contact_Us_Customer = () => {
                 className="name_input"
                 type="text"
                 placeholder="Enter your Message"
+                name="message"
+                required
+                value={newFeedback.message}
+                onChange={OnChange}
                 style={{ fontFamily: "sans-serif", paddingTop: "4px" }}
               />
             </div>
 
             <div className="button">
               <Button
-                type="submit"
                 variant="contained"
                 style={{
                   backgroundColor: "red",
@@ -109,6 +154,9 @@ const Contact_Us_Customer = () => {
                   paddingRight: "6%",
                   borderRadius: "5px",
                   textTransform: "uppercase",
+                }}
+                onClick={() => {
+                  navigate("/");
                 }}
               >
                 Cancel
