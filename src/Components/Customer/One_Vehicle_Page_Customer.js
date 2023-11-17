ef8a206503ca85ab94999ac14d619b180ea6b35d
Zaeem <zaeemahmad2603@gmail.com>
2023-11-17 10:35:35 +0500

    Develop fully integrated page to display the one vehicle for customer

--- src/Components/Customer/One_Vehicle_Page_Customer.js
+++ src/Components/Customer/One_Vehicle_Page_Customer.js
@@ -1,42 +1,68 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import Button from "@mui/material/Button";
 import image from "../../Images/one.jpg";
+import image2 from "../../Images/tow.jpeg";
+import image3 from "../../Images/home_page_car.png";
 import "../../One_Vehicle_Page.css";
+import { useNavigate } from "react-router-dom";
+import ReactDOM from "react-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import { VehicleContext } from "../../Context/AllContexts";
 
 const One_Vehicle_Page_Customer = () => {
+  const navigate = useNavigate();
+
+  const Context = useContext(VehicleContext);
+  const { vehicleForOneDetailedPage, setVehicleForOneDetailedPage } = Context;
+
+  useEffect(() => {
+    if (localStorage.getItem("customerToken")) {
+      
+    } else {
+      alert("You have to Logged In into the system.");
+      navigate("/Login");
+    }
+  }, []);
+
   return (
     <div className="Main_Page">
       <div className="image_portion">
-        <img src={image} alt="Image is not found." className="Image" />
+        <Carousel infiniteLoop>
+          {vehicleForOneDetailedPage.image.map((img) => {
+            return (
+              <div>
+                <img src={img} />
+              </div>
+            );
+          })}
+        </Carousel>
       </div>
       <div className="information_portion">
         <div className="first_line">
-          <h2 className="brand">Toyoto Model</h2>
-          <h4 className="year">2022</h4>
+          <h2 className="brand">{vehicleForOneDetailedPage.brand.name}</h2>
+          <h4 className="year">{vehicleForOneDetailedPage.year}</h4>
         </div>
-        <p className="description">
-          This is the toyota model car whose mileage is very good and it also
-          has the music system and also a wifi network system.
-        </p>
+        <p className="description">{vehicleForOneDetailedPage.description}</p>
         <div className="second_line">
           <h3 className="BrandName">Brand : </h3>
-          <h3>Toyota</h3>
+          <h3>{vehicleForOneDetailedPage.brand.name}</h3>
         </div>
         <div className="second_line">
           <h3 className="pricename">Price per Day : </h3>
-          <h3>$200</h3>
+          <h3>{vehicleForOneDetailedPage.pricePerDay}</h3>
         </div>
         <div className="second_line">
           <h3 className="discountname">Discount on Car : </h3>
-          <h3>$0</h3>
+          <h3>{vehicleForOneDetailedPage.discount}</h3>
         </div>
         <div className="second_line">
           <h3 className="statusname">Availability Status : </h3>
-          <h3>Available</h3>
+          <h3>{vehicleForOneDetailedPage.status}</h3>
         </div>
         <div className="second_line">
           <h3 className="platename">Plate Number : </h3>
-          <h3>1234</h3>
+          <h3>{vehicleForOneDetailedPage.plateNumber}</h3>
         </div>
         <div className="button_line">
           <Button
@@ -49,6 +75,9 @@ const One_Vehicle_Page_Customer = () => {
               paddingLeft: "8%",
               paddingRight: "8%",
               fontSize: "16px",
+            }}
+            onClick={() => {
+              navigate("/");
             }}
           >
             Cancel
@@ -63,6 +92,14 @@ const One_Vehicle_Page_Customer = () => {
               paddingLeft: "8%",
               paddingRight: "8%",
               fontSize: "16px",
+            }}
+            onClick={() => {
+              if(vehicleForOneDetailedPage.status === 'Not Available'){
+                alert("This car is not available this time.")
+              }
+              else{
+                navigate("/customer/reservationvehicle");
+              }
             }}
           >
             Reserve Car
