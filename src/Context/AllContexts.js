7262c3dbcef4dd169aa3ee00a94a7f03781199df
Zaeem <zaeemahmad2603@gmail.com>
2023-11-18 15:20:36 +0500

    Create all contexts for application front end and integration

---
+++ src/Context/AllContexts.js
@@ -0,0 +1,34 @@
+import { createContext } from "react";
+
+const EmployeeContext = createContext();
+const BrandContext = createContext();
+const VehicleContext = createContext();
+const CustomerContext = createContext();
+const ReservationContext = createContext();
+const FeedbackContext = createContext();
+
+export { EmployeeContext, BrandContext, VehicleContext, CustomerContext, ReservationContext, FeedbackContext }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
