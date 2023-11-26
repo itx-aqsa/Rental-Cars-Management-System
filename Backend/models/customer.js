479742fae32bcf9f5aa170ba7dbc626a08f3869b
Zaeem <zaeemahmad2603@gmail.com>
2023-11-26 12:52:43 +0500

    Create the audit tables for customer

--- Backend/models/customer.js
+++ Backend/models/customer.js
@@ -37,5 +37,62 @@ const customerSchema = new Schema({
     },
 }, { timestamps: true })
 
+const auditCustomerSchema = new Schema({
+    customerId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    operation: {
+        type: String,
+        required: true
+    },
+    previousValues: {
+        type: Object, // You might need to adjust this based on your data types
+    },
+    currentValues: {
+        type: Object,
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const AuditCustomer = mongoose.model('AuditCustomer', auditCustomerSchema);
+
+customerSchema.pre('findOneAndUpdate', async function (next) {
+    try {
+        // Capture the original document before the update
+        this._originalDoc = await this.model.findOne(this._conditions);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+customerSchema.post('findOneAndUpdate', async function (result) {
+    try {
+        // console.log("I am here.")
+        const customerId = this._originalDoc._id;
+        const previousValues = this._originalDoc.toObject(); // Convert to plain object
+        // console.log(previousValues) 
+        const currentValues = this._update.$set; // Use this._update.$set to get modifications
+        // console.log(currentValues)
+        await AuditCustomer.create({
+            customerId,
+            operation: 'update',
+            previousValues,
+            currentValues
+        });
+    } catch (error) {
+        console.error("Error in post update middleware:", error);
+    }
+});
+
+
+
+
+
+
 const Customer = mongoose.model('Customer', customerSchema)
 module.exports = Customer
